99b3cd449df4b606b4735b301e915440c0c7074f
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-05-15 14:39:47 +0900

    /homeのメモタイトルをクリックすることで/detailに遷移し、サンプルデータの中身がひとつ表示されるようにした。

--- frontend/src/routes/detail/+page.svelte
+++ frontend/src/routes/detail/+page.svelte
@@ -1,13 +1,33 @@
 <script>
   import { memoData } from "$lib/stores/dataStore";
+  import { onMount } from "svelte";
+  
+  let title = "";
+  let body = "";
+  let references = "";
 
-  console.log(memoData.title);
+  onMount(() => {
+    const unsubscribe = memoData.subscribe(value => {
+      // valueはオブジェクトの配列で与えられる。
+      /*
+      TODO： ページごとに対応したオブジェクトにアクセスできるようにする。
+      例： "テスト1"をクリックするとテスト1の詳細な中身が、
+      "テスト2"をクリックするとテスト2の詳細な中身が表示される。
+      */
+      title = value[0].title;
+      body = value[0].body;
+      references = value[0].references;
+    });
+    return () => {
+      unsubscribe();
+    }
+  });
   
 </script>
 
 <div class="detail">
-  <h1>タイトル：{memoData.title}</h1>
-  <p>{memoData.body}</p>
+  <h1>タイトル：{title}</h1>
+  <p>{body}</p>
   <h2>参考文献：</h2>
-  <p>{memoData.references}</p>
+  <p>{references}</p>
 </div>
