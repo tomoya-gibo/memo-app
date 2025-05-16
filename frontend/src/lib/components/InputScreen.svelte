77e95a510ce1e61cddab3e5eda142d11bdf6785c
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-05-16 14:50:34 +0900

    ユーザからの入力を受け付けるページのUIを定義したInputScreenコンポーネントを作成し、UIが表示されるようにした。

---
+++ frontend/src/lib/components/InputScreen.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { memoData } from "$lib/stores/dataStore";
+  
+  export let title;
+  export let body;
+  export let references;
+
+  //保存を実行するイベントハンドラ
+  export let onSave;
+</script>
+
+<div class="input">
+  <p>タイトル：</p>
+  <input type="text" name="title" value={title}>
+
+  <p>本文：</p>
+  <input type="text" name="body" value={body}>
+
+  <p>参考文献：</p>
+  <input type="text" name="references" value={references}>
+</div>
+
+<button on:click={onSave}>保存</button>
--- frontend/src/routes/edit/+page.svelte
+++ frontend/src/routes/edit/+page.svelte
@@ -1,12 +1,13 @@
 <script>
   import { memoData } from "$lib/stores/dataStore";
+  import InputScreen from "$lib/components/InputScreen.svelte";
 
   // memoData(store)のそれぞれの値にアクセスする。
   let title = $memoData.title;
   let body = $memoData.body;
   let references = $memoData.references;
   
-  function saveData() {
+  function updateData() {
     // TODO: ボタンを押した時に、入力されたメモの内容を保存する処理を記述
   };
 
@@ -14,3 +15,10 @@
   console.log(title, body, references);
   
 </script>
+
+<InputScreen
+  {title}
+  {body}
+  {references}
+  onSave={updateData}
+/>
