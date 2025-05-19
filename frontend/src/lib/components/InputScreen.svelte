8af68526ca1c70e0e2674299c23e948df0a907ed
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-05-19 11:39:10 +0900

    ユーザが入力したデータを保存し、/detailで更新内容が反映されるようにした。

--- frontend/src/lib/components/InputScreen.svelte
+++ frontend/src/lib/components/InputScreen.svelte
@@ -5,19 +5,30 @@
   export let body;
   export let references;
 
-  //保存を実行するイベントハンドラ
+  // 保存を実行するイベントハンドラを受け取る
   export let onSave;
+
+  /*
+  /detailのsubscribeで受け取るvalueの型がオブジェクトを想定しているため、
+  ユーザから受け取った入力をオブジェクトに変換する。
+  */
+  let inputData = {
+    title: title,
+    body: body,
+    references: references
+  }
+
 </script>
 
 <div class="input">
   <p>タイトル：</p>
-  <input type="text" name="title" value={title}>
+  <input type="text" name="title" bind:value={inputData.title}>
 
   <p>本文：</p>
-  <input type="text" name="body" value={body}>
+  <input type="text" name="body" bind:value={inputData.body}>
 
   <p>参考文献：</p>
-  <input type="text" name="references" value={references}>
+  <input type="text" name="references" bind:value={inputData.references}>
 </div>
 
-<button on:click={onSave}>保存</button>
+<button on:click={() => onSave(inputData)}>保存</button>
--- frontend/src/routes/edit/+page.svelte
+++ frontend/src/routes/edit/+page.svelte
@@ -2,13 +2,20 @@
   import { memoData } from "$lib/stores/dataStore";
   import InputScreen from "$lib/components/InputScreen.svelte";
 
-  // memoData(store)のそれぞれの値にアクセスする。
+  /*
+  入力フォームの初期値を取得するため、
+  memoData(store)のそれぞれの値にアクセスする。
+  */
   let title = $memoData.title;
   let body = $memoData.body;
   let references = $memoData.references;
   
-  function updateData() {
-    // TODO: ボタンを押した時に、入力されたメモの内容を保存する処理を記述
+  function updateData(inputData) {
+    memoData.set(inputData)
+
+    // デバッグ用
+    console.log(inputData.title, inputData.body, inputData.references);
+    
   };
 
   // デバッグ用
