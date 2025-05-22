443e511f9b9e049dd2f7ebda4bfcc212399dbac0
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-05-22 14:44:14 +0900

    不要なローカル変数を削除し、タイトルが空文字の場合にサンプルデータをsetするように修正した。

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -8,20 +8,16 @@
     body: "テスト1の本文",
     references: "テスト1の参考文献"
   };
-
-  let title = $memoData.title;
-  let body = $memoData.body;
-  let references = $memoData.references;
   
   let datas = {
-    title: title,
-    body: body,
-    references: references
+    title: $memoData.title,
+    body: $memoData.body,
+    references: $memoData.references
   };
 
-  // storeの値が初期値(空文字)かを判定する。空文字であればtrueを返す。
+  // storeのタイトルが初期値(空文字)かを判定する。空文字であればtrueを返す。
   function isEmpty(obj) {
-    return !obj.title || !obj.body || !obj.references;
+    return !obj.title;
   }
 
   console.log("isEmpty:", isEmpty(datas));
@@ -32,15 +28,6 @@
     console.log("onMountの先頭でのdatas:", datas);
     
     console.log("datasのプロパティの個数(onMount直後):", Object.keys(datas).length);
-
-    // store値が初期値の場合はサンプルデータを初期値として適用。
-    if (isEmpty(datas)) {
-      console.log("ifブロックの中");
-      
-      memoData.set(sampleDatas);
-
-      console.log("set後のdatasの値", datas);
-    }
     
     const unsubscribe = memoData.subscribe((value) => {
       /*title = value.title;
@@ -57,6 +44,18 @@
 
     console.log("datasのプロパティの個数(subscribe後):", Object.keys(datas).length);
     console.log("subscribe後のdatas:", datas);
+
+    /*
+    storeのtitleが初期値の場合はサンプルデータを初期値として適用。
+    subscribeで変更を購読した後に判定を行う。
+    */
+    if (isEmpty(datas)) {
+      console.log("ifブロックの中");
+      
+      memoData.set(sampleDatas);
+
+      console.log("set後のdatasの値", datas);
+    }
 
     return () => {
       unsubscribe();
