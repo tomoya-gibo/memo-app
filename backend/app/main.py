7fb1d541a30f3fbaa0625b73c67c5e8f591805a3
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-05-26 13:56:55 +0900

    メモデータの初期値をバックエンドに記述した。

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# メモデータの初期値(サンプルデータ)
+memoData = {
+  "title": "テスト1",
+  "body": "テスト1の本文",
+  "references": "テスト1の参考文献"
+}
+
 @app.get("/")
 async def root():
   return {"message": "Hello World"}
