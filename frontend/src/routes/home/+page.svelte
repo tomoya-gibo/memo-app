d1c14e473709b06f326bbaf701c04c50bbce2d9d
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-06-03 11:50:37 +0900

    /homeのsubscribeを削除し、fetchSetDataを実行することで、storeではなくバックエンドからデータを取得するようにした。

--- frontend/src/routes/home/+page.svelte
+++ frontend/src/routes/home/+page.svelte
@@ -17,8 +17,6 @@
   // 非同期が必要な処理だけをまとめて関数化する。
   async function fetchSetData() {
     datas = await fetchData();
-    // TODO: storeが不要になったら削除する。
-    //memoData.set(fetchedData);
     console.log("homeのfetch後のdatas", datas);
   }
 
@@ -30,37 +28,26 @@
     console.log("onMountの先頭でのdatas:", datas);
     
     console.log("datasのプロパティの個数(onMount直後):", Object.keys(datas).length);
-    
-    const unsubscribe = memoData.subscribe((value) => {
-      /*title = value.title;
-      body = value.body;
-      references = value.references;*/
-      console.log("subscribeの中");
-      console.log("value:", value);
-      
-      datas = value;
-
-      console.log("subscribeの中のdatas:", datas);
-      
-    });
 
     console.log("datasのプロパティの個数(subscribe後):", Object.keys(datas).length);
     console.log("subscribe後のdatas:", datas);
+
+    fetchSetData();
 
     /*
     storeのtitleが初期値の場合はサンプルデータを初期値として適用。
     subscribeで変更を購読した後に判定を行う。
     */
-    if (isEmpty(datas)) {
+    /*if (isEmpty(datas)) {
       console.log("ifブロックの中");
 
       fetchSetData();
 
       console.log("set後のdatasの値", datas);
-    }
+    }*/
 
     return () => {
-      unsubscribe();
+      //unsubscribe();
       // デバッグ用
       console.log(datas);
       console.log(typeof datas);
